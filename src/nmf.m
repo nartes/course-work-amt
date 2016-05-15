280ad520bde7a1a7ab3b6206170f5ce5a90e5c6a
Sergei Sinyak <serega.belarus@gmail.com>
2016-05-15 18:23:02 +0300

    Replace symlinks with real files

--- src/nmf.m
+++ src/nmf.m
@@ -1,1 +1,117 @@
-/home/nartes/Documents/BSU/BSU 6th term/DigitalSignalProcessing/nmf_octave/nmf.m
+function nmf
+  pkg load all
+
+  nmf_test();
+
+  info = audioinfo(
+    #'res/piano_samples_ogg/a4.ogg'
+    #'res/la-do-mi-la.wav'
+    #'res/sample1.wav'
+    'res/sample2.0.wav'
+    #'res/sample3.wav'
+  );
+
+  L = 4096;
+  MAX_WINDOWS = 10000;
+  raw_signal = mean(audioread(info.Filename)');
+  s = raw_signal(1:min(length(raw_signal), L * MAX_WINDOWS)');
+
+  printf("File name is %s\n", info.Filename);
+  printf ("Signal length is %d (approx. %ds)\n",
+      length(s), length(s) / info.SampleRate);
+  printf ("Num of channels is %d\n", info.NumChannels);
+  printf ("Window length is %d\n", L);
+
+  V = getspectrum(s, L);
+  #[W, H, d] = innernmf(V, 4);
+  [n, m] = size(V);
+  r = 7;
+  [W, H] = nmf_pg(V, 'Winit', rand(n, r), 'Hinit', rand(r, m));
+
+  subplot(121);
+  mesh(W);
+  subplot(122);
+  mesh(H);
+endfunction
+
+function nmf_test
+  # V = W * H
+
+  n = 2048;
+  r = 4;
+  m = 22;
+
+  W = rand(n, r);
+  H = ones(r, m);
+
+  V = W * H;
+
+  [W1, H1, d] = innernmf(V, r);
+
+  printf("Error is %d\n", d);
+
+  if (d > 0.1)
+    error("incorrect nmf algorithm!");
+  end
+endfunction
+
+function v = getspectrum(s, L)
+
+# usage: getspectrum(s, L)
+#
+# This function generates spectrogram
+# of a signal s with a time window
+# of a size L
+#
+# s - audiosignal
+# L - time window size
+#
+# return: matrix V of a size (L / 2 + 1) x (length(s) / L)
+
+  x = @(t) abs (fft (s (t:t + L - 1))) (1:L / 2 + 1);
+  m = idivide(length(s), L);
+  res = ones(L / 2 + 1, m);
+
+  for i = 1:m
+    res(:, i) = x((i - 1) * L + 1);
+  end
+
+  v = res;
+endfunction
+
+function [W,H,d] = innernmf(V, r = 2)
+  [n, m] = size(V);
+
+  PERGRADIENT = 1000;
+  count = 0;
+  MAX_ITER = 10000;
+
+  do
+    W = rand(n, r); H = rand(r, m);
+
+    #updateH = @(V,H,W) H .* sum(W)' .* sum(V) ./ sum(W*H) ./ sum(W)';
+    #updateW = @(V,H,W) W .* sum(H, 2)' .* sum(V, 2) ...
+    #            ./ sum(W * H, 2) ./ sum(H, 2)';
+    updateH = @(V, H, W) H .* (W' * V) ./ (W' * W * H);
+    updateW = @(V, H, W) W .* (V * H') ./ (W * H * H');
+
+    D = @(A,B) sum(sum(A .* log (A ./ B) .- A .+ B));
+    N = @(A, B) sum(sumsq(A .- B));
+
+    #while (D(V, W * H) > 0.1)
+    printf("N is %d\n", N(V, W * H));
+    while (N(V, W * H) > 0.1)
+      H = updateH(V, H, W);
+      W = updateW(V, H, W);
+
+      ++count;
+      if (rem(count, PERGRADIENT) == 0)
+        break;
+      end
+      printf("N is %d\n", N(V, W * H));
+    endwhile
+
+    d = N(V, W * H);
+  until (d < 0.1 || count == MAX_ITER)
+  printf("Iter is %d\n", count);
+endfunction
--- src/test2.m
+++ src/test2.m
@@ -1,1 +1,71 @@
-/home/nartes/Documents/BSU/BSU 5th term/DSP/test2.m
+#file_name='shelter.wav';
+#file_name='track.wav';
+file_name='la-do-mi-la.wav';
+#file_name='pepel.wav';
+#file_name='samples/a2m.ogg';
+song=audioread(file_name);
+Fs=audioinfo(file_name).SampleRate;
+
+#{
+Fs=44100; % sampling frequency
+f0=167; % Hz frequency of sinusoid
+l=0; r=10; % time period in seconds
+t=[l:1/Fs:r]; % discrete time
+song=0.8*sin(2*pi*f0*t); % generated signal
+#}
+
+#l=max(1,round(Fs*0.133));
+#r=min(length(song),round(Fs*0.2));
+l=1;
+r=length(song);
+lb=1;
+hb=300;
+
+x=song(l:r);
+
+MULT_SIZE=4;
+MULT_TIME=16;
+
+Size=2048*MULT_SIZE;
+
+WP=[];
+WA=[];
+NMP=[];
+NMA=[];
+
+for k=1:Size/MULT_TIME:length(x)
+  bs=postpad(x(k:min(k+Size,length(x))),Size);
+
+  c=fft(postpad(bs,2*Size))/Size;
+  hc=c(1:Size);
+  nmp=20*log10(abs(hc));
+  WP=(0:length(nmp)-1)*Fs/length(nmp)/2;
+  NMP=[NMP; nmp];
+
+
+  [ha,wa] = freqz(bs,1,length(bs),Fs);
+  WA=wa;
+  nma=20*log10(abs(ha));
+  NMA=[NMA; nma'];
+end
+
+
+#{
+[lt,nw]=size(NMA);
+HB=round(nw*hb*2/Fs);
+LB=round(nw*lb*2/Fs)+1;
+T=(1:lt)*Size/Fs/MULT_TIME;
+
+#subplot(212);
+figure(1); mesh(T,WA(LB:HB),NMA(:,LB:HB)');
+#}
+
+[lt,nw]=size(NMP);
+HB=round(nw*hb*2/Fs);
+LB=round(nw*lb*2/Fs)+1;
+T=(0:lt-1)*Size/Fs/MULT_TIME;
+#subplot(211);
+figure(2); mesh(T,WP(LB:HB),NMP(:,LB:HB)');
+
+figure(3); plot(WP(LB:HB),mean(NMP(:,LB:HB)));
+#set(gca,'xscale','log'); output_precision(0);
