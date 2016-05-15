4bd1231fcaec779e89f265be13e74dff42a2fde2
Sergei Sinyak <serega.belarus@gmail.com>
2016-05-15 18:03:57 +0300

    Refactor tabs to spaces

--- src/simple_signal_1.m
+++ src/simple_signal_1.m
@@ -1,19 +1,19 @@
 function simple_signal_1
-	signal = @(a, b, g, d, t) gate(a * t) .* sin(g * t) + gate(b * t) .* sin(d * t);
+  signal = @(a, b, g, d, t) gate(a * t) .* sin(g * t) + gate(b * t) .* sin(d * t);
 
-	sample_rate = 44100;
-	duration = 10; % seconds
+  sample_rate = 44100;
+  duration = 10; % seconds
 
-	t = linspace(0, duration, round(duration * sample_rate));
+  t = linspace(0, duration, round(duration * sample_rate));
 
-	a = 2 * pi / duration * 3;
-	b = 2 * pi / duration * 2;
-	g = 440 * 2 * pi;
-	d = (440 * pow2(-5 / 12)) * 2 * pi;
+  a = 2 * pi / duration * 3;
+  b = 2 * pi / duration * 2;
+  g = 440 * 2 * pi;
+  d = (440 * pow2(-5 / 12)) * 2 * pi;
 
-	s = signal(a, b, g, d, t);
+  s = signal(a, b, g, d, t);
 
-	audiowrite('/tmp/audio.wav', s, sample_rate);
+  audiowrite('/tmp/audio.wav', s, sample_rate);
 
-	system('mplayer /tmp/audio.wav');
+  system('mplayer /tmp/audio.wav');
 end
