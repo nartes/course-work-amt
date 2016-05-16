f69cc4c669449f07b3d572e1ef9758d7c746f785
Sergei Sinyak <serega.belarus@gmail.com>
2016-05-16 21:14:46 +0300

    Make image generating on demand

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
-octave --eval 'simple_analysis_1'
+#!/usr/bin/zsh
+
+if [[ ! -e build/spectrum.png ]] then
+	octave --eval 'simple_analysis_1(false)'
+fi
+
 pdflatex -output-directory=build/ doc/report_en.tex
 #pdflatex -output-directory=build/ doc/report.tex
