d2f1ea6600610b316455273d42fe7c6b88a998ba
Siarhei Siniak <serega.belarus@gmail.com>
2016-12-15 21:06:10 +0300

    Add analysis of organ phrase
      1. Add peak detection and parabollic interpolation
    
      2. Apply nmf and plot W and H matrices

---
+++ src/realaudio_nmf_post.m
@@ -0,0 +1,105 @@
+function realaudio_nmf_post
+  pkg('load', 'linear-algebra');
+
+  fname = 'res/organ.wav';
+
+  info = audioinfo('res/organ.wav');
+
+  FS = info.SampleRate;
+  MFH = 6000;
+  M = 1024;
+  HS = floor(M * 0.25);
+  N = 4096;
+  MFS = floor(MFH / FS * N);
+
+
+  TL = 0.0;
+  TR = 5.5;
+  SL = floor(TL * FS) + 1;
+  SR = floor(TR * FS) + 1;
+
+  x = mean(audioread(info.Filename), 2)(SL : SR);
+  [y, c] = stft(x, M, HS, N / 2);
+  L = size(y, 2);
+
+  sy = y(1 : MFS, :);
+
+  test_peaks_detector(sy);
+
+
+  plot_grayscale_spectrogram(sy);
+
+  R = 4;
+
+  [W, H] = innernmf(abs(sy), R, 1e-2);
+
+  plot_h_matrix(L, M, HS, FS, TL, R, H);
+  plot_w_matrix(FS, N, MFS, R, W);
+endfunction
+
+function test_peaks_detector(sy)
+  sl = abs(sy(:, 100))';
+
+  plot(sl);
+
+  peaks = peaks_detector(sl);
+  plot(1 : length(sl), sl, 'r', peaks(:, 1), peaks(:, 2), 'g');
+endfunction
+
+function [peaks] = peaks_detector(x)
+  ind = find(x(2 : end - 1) > x(1 : end - 2) &
+             x(2 : end - 1) > x(3 : end)) + 1;
+
+  peaks = zeros(length(ind), 2);
+
+  for i = 1 : length(ind)
+    bin = ind(i);
+    px = [bin - 1, bin, bin + 1];
+    py = x(px);
+    p = polyfit(px, py, 2);
+    dp = polyder(p);
+    r = roots(dp);
+    mx = r(r > bin - 1 & r < bin + 1);
+    abin = polyval(p, mx)(1);
+    peaks(i, :) = [mx abin];
+  endfor
+endfunction
+
+function pitch_estimator(FS, N, R, W)
+  peaks = cell(1, R);
+  for k = 1 : R
+    x = W(:, k)';
+    peaks(k) = peaks_detector(x);
+  endfor
+endfunction
+
+function plot_grayscale_spectrogram(sy)
+  figure();
+  imshow(flipud(abs(sy)));
+endfunction
+
+function plot_3d_mesh(L, MFS, sy)
+  [ftime, ffreq] = meshgrid(1 : L, 1 : MFS);
+  mesh(ftime, ffreq, abs(sy));
+endfunction
+
+function plot_w_matrix(FS, N, MFS, R, W)
+  fhz = (1 : MFS) / N * FS;
+
+  figure();
+  for k = 1 : R
+    subplot(R, 1, k);
+    plot(fhz, W(:, k)');
+    xlabel('Hz'); ylabel('amplitude');
+  endfor
+endfunction
+
+function plot_h_matrix(L, M, HS, FS, TL, R, H)
+  tsec = ((0 : L - 1)  * HS + M / 2) / FS + TL;
+  figure();
+  for k = 1 : R
+    subplot(R, 1, k);
+    plot(tsec, H(k, :));
+    xlabel('sec'); ylabel('amplitude');
+  endfor
+endfunction
